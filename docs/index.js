d96bfb41cee09e4a2cb8c887662c3d8932980b95
elvisdamm <elvisdaniel.menendezm@gmail.com>
2022-05-18 12:57:14 +0200

    index.js

---
+++ docs/index.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+        autor: "Elvis Daniel Menéndez Muñiz",
+        hour: "12:00",
+        title: "Index",
+        pageType: "Single Page"
+    })
