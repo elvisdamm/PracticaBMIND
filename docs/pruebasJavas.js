c85142f9e5522df7cc4f2c8eacd44b9f49a4b614
elvisdamm <elvisdaniel.menendezm@gmail.com>
2022-05-18 14:01:26 +0200

    hg

---
+++ docs/pruebasJavas.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+        pageAuthor: 'Nuria Rodríguez Álvarez',
+        pageHour: '18:00',
+        pageTitle: 'JavaScript',
+        pageType: 'Single Page'
+    })
---
+++ docs/pruebasJquery.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || []
+    window.dataLayer.push({
+        pageAuthor: 'Pablo López Rodríguez',
+        pageHour: '08:00',
+        pageTitle: 'JQuery',
+        pageType: 'Double Page'
+    })
